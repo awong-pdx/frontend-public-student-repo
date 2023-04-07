27f813c88d92bfc52c1159d7d32ff46a34e3477e
Alessandra Wong <awong@pdx.edu>
2023-04-07 15:18:00 -0700

    Added checkPalindrome function
    Implemented checkPalindrome to check any Number to
    determine if it is a palindrome.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -7,7 +7,25 @@ function handleInput(input) {
   console.log(input);
   if (input.data > 0) {
     result.textContent = "you entered a positive number";
+    // check for palindrome
   } else {
     result.textContent = "error: please enter a positive number";
   }
+}
+
+function checkPalindrome(number) {
+  let isPalindrome = false;
+
+  if (Number.isInteger(number)) {
+    number = number.toString();
+    let i = 0;
+    let len = number.length;
+    isPalindrome = true;
+    while (i < len - i && isPalindrome) {
+      if (number[i] !== number[len - 1 - i]) isPalindrome = false;
+      i++;
+    }
+  }
+
+  return isPalindrome;
 }
