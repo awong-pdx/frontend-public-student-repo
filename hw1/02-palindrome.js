6959c26d3ee4efba81ed737c499c021921ca3e21
Alessandra Wong <awong@pdx.edu>
2023-04-07 14:06:24 -0700

    Started Exercise 02 - Palindrome
    Added a handleInput function to check for a palindrome
    and implemented a simple check for
    positive number input.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,13 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
+const result = document.querySelector(".result");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
+
+function handleInput(input) {
+  console.log(input);
+  if (input.data > 0) {
+    result.textContent = "you entered a positive number";
+  } else {
+    result.textContent = "error: please enter a positive number";
+  }
+}
