7613d4650b036bca38f91bda1151a2a351596cbd
Alessandra Wong <awong@pdx.edu>
2023-04-15 16:42:39 -0700

    Rough functions for input word search, highlight
    Started working on 03-Count and created
    a function to highlight text and
    another handleInput function.
    Need to refine both functions

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,55 @@
 // Add your code here
 
-input.addEventListener('keydown', handleKeyDown);
+const searchText = document.querySelector('.search-text');
+const textInput = document.querySelector('.word-input');
+const originalTextContent = searchText.innerHTML;
+
+const highlight = function findAndHighlightInputWordInText(
+  inputWord,
+  textElement,
+) {
+  let newTextElementInnerHTML = textElement.innerHTML;
+
+  if (typeof inputWord === 'string' && inputWord !== '') {
+    const wordReplacement = `<span class='bg-warning'>${inputWord}</span>`;
+    newTextElementInnerHTML = textElement.innerHTML
+      .split(inputWord)
+      .join(wordReplacement);
+  }
+
+  return newTextElementInnerHTML;
+};
+
+// let currentWord = '';
+// const handle = function handleKeyDown(input) {
+//   console.log(`current key press: ${input.key}`);
+//   console.log(`current input: ${textInput.value}`);
+//   if (/^[a-zA-Z]$/.test(input.key)) {
+//     // should probably declare separately
+//     currentWord += input.key;
+//   } else if (textInput.value === '') currentWord = '';
+//   else if (input.key === 'Backspace' && currentWord.length > 0) {
+//     currentWord = currentWord.substring(0, currentWord.length - 1);
+//   }
+//   console.log(`currentword: ${currentWord}`);
+//   const newInnerHTML = highlight(currentWord, searchText);
+//   if (searchText.innerHTML !== newInnerHTML) {
+//     searchText.innerHTML = newInnerHTML;
+//   } else {
+//     searchText.innerHTML = originalTextContent;
+//   }
+// };
+
+const handleIn = function handleInput(event) {
+  const { value } = event.target;
+  console.log(`The value found from handling input: ${value}`);
+  const newInnerHTML = highlight(value, searchText);
+  if (searchText.innerHTML !== newInnerHTML) {
+    searchText.innerHTML = newInnerHTML;
+  } else {
+    searchText.innerHTML = originalTextContent;
+  }
+};
+
+// textInput.addEventListener('keydown', handle);
+textInput.addEventListener('input', handleIn);
