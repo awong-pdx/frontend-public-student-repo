ffa4b0832a92534d42b6cabb5858f4b557efb593
Alessandra Wong <awong@pdx.edu>
2023-04-15 17:07:11 -0700

    Updated highlight function
    Added regex check to exclude partial word matches

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -4,46 +4,22 @@ const searchText = document.querySelector('.search-text');
 const textInput = document.querySelector('.word-input');
 const originalTextContent = searchText.innerHTML;
 
-const highlight = function findAndHighlightInputWordInText(
-  inputWord,
-  textElement,
-) {
-  let newTextElementInnerHTML = textElement.innerHTML;
+const highlight = function findAndHighlightInputWordInText(inputWord, text) {
+  let newTextInnerHTML = text;
 
   if (typeof inputWord === 'string' && inputWord !== '') {
     const wordReplacement = `<span class='bg-warning'>${inputWord}</span>`;
-    newTextElementInnerHTML = textElement.innerHTML
-      .split(inputWord)
-      .join(wordReplacement);
+    const inputWordSpaceCheck = new RegExp(`\\b${inputWord}\\b`, 'g');
+    newTextInnerHTML = text.split(inputWordSpaceCheck).join(wordReplacement);
   }
 
-  return newTextElementInnerHTML;
+  return newTextInnerHTML;
 };
-
-// let currentWord = '';
-// const handle = function handleKeyDown(input) {
-//   console.log(`current key press: ${input.key}`);
-//   console.log(`current input: ${textInput.value}`);
-//   if (/^[a-zA-Z]$/.test(input.key)) {
-//     // should probably declare separately
-//     currentWord += input.key;
-//   } else if (textInput.value === '') currentWord = '';
-//   else if (input.key === 'Backspace' && currentWord.length > 0) {
-//     currentWord = currentWord.substring(0, currentWord.length - 1);
-//   }
-//   console.log(`currentword: ${currentWord}`);
-//   const newInnerHTML = highlight(currentWord, searchText);
-//   if (searchText.innerHTML !== newInnerHTML) {
-//     searchText.innerHTML = newInnerHTML;
-//   } else {
-//     searchText.innerHTML = originalTextContent;
-//   }
-// };
 
 const handleIn = function handleInput(event) {
   const { value } = event.target;
   console.log(`The value found from handling input: ${value}`);
-  const newInnerHTML = highlight(value, searchText);
+  const newInnerHTML = highlight(value, originalTextContent);
   if (searchText.innerHTML !== newInnerHTML) {
     searchText.innerHTML = newInnerHTML;
   } else {
