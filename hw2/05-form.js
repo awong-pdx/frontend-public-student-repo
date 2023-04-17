03c8383a997292cb671ad7f484a640d7892454be
Alessandra Wong <awong@pdx.edu>
2023-04-16 18:24:18 -0700

    Updated form handling to use form names
    Added name attributes to all form elements
    and used attributes in form capture/logging

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -2,43 +2,39 @@
 const registrationForm = document.querySelector('.registration-form');
 
 const logForm = function captureAndLogRegistrationForm(event) {
-  const { target } = event;
-  const name = target[0].value;
-  const email = target[1].value;
-  const registrationStatus = target[2].value;
-  // const coursesProgLangs = target[4];
-  // const coursesOpSys = target[5];
-  // const coursesFullStack = target[6];
-  let langsTaken = [target[4], target[5], target[6]];
-  langsTaken = langsTaken
+  const { fullName, email, registrationStatus, coursesTaken, anythingElse } =
+    event.target;
+  // const { target } = event;
+  // const name = target[0].value;
+  // const email = target[1].value;
+  // const registrationStatus = target[2].value;
+  //  let coursesNamesTaken = [target[4], target[5], target[6]];
+  // let coursesNamesTaken = [coursesTaken[0], coursesTaken[1], coursesTaken[2]];
+  let coursesNamesTaken = [...coursesTaken];
+  coursesNamesTaken = coursesNamesTaken
     .filter((lang) => lang.checked === true)
     .map((lang) => lang.labels[0].textContent);
-  langsTaken = langsTaken.join(', ');
-  const anythingElse = target[7].value;
-  // console.log(
-  //   name,
-  //   email,
-  //   registrationStatus,
-  //   coursesProgLangs.checked,
-  //   coursesOpSys.checked,
-  //   coursesFullStack.checked,
-  //   anythingElse,
-  // );
+  coursesNamesTaken = coursesNamesTaken.join(', ');
+  // const anythingElse = target[7].value;
 
   console.log(`
-  Full name: ${name}
-  Email: ${email}
-  Registration status: ${registrationStatus}
-  Courses taken: ${langsTaken}
-  Additional Comments: ${anythingElse}
+  ${fullName.value !== '' ? `Full name: ${fullName.value}` : ''}
+  ${email.value !== '' ? `Email: ${email.value}` : ''}
+  ${
+    registrationStatus.value !== ''
+      ? `Registration status: ${registrationStatus.value}`
+      : ''
+  }
+  ${coursesNamesTaken.length > 0 ? `Courses taken: ${coursesNamesTaken}` : ''}
+  ${
+    anythingElse.value !== ''
+      ? `Additional Comments: ${anythingElse.value}`
+      : ''
+  }
   `);
-  // console.log(typeof target.0);
-  // console.log(target[0].value);
-  // console.log(name, email, registrationStatus, coursesTaken, anythingElse);
-  // console.log(target[0].value);
-  event.preventDefault();
-};
 
-// const formatLog = function formatLogOutp
+  event.preventDefault();
+  console.log(event.target);
+};
 
 registrationForm.addEventListener('submit', logForm);
