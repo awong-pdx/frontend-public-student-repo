a24f742cf8c27e98e2525609a044fb52fbef9977
Alessandra Wong <awong@pdx.edu>
2023-04-27 12:48:18 -0700

    Fixed indentation and other style issues

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -2,9 +2,14 @@ const registrationForm = document.querySelector('.registration-form');
 
 const logForm = function captureAndLogRegistrationForm(event) {
   const {
-    fullName, email, registrationStatus, coursesTaken, anythingElse,
+    fullName,
+    email,
+    registrationStatus,
+    coursesTaken,
+    anythingElse,
   } = event.target;
   let coursesNamesTaken = [...coursesTaken];
+
   coursesNamesTaken = coursesNamesTaken
     .filter((lang) => lang.checked === true)
     .map((lang) => lang.labels[0].textContent);
@@ -13,17 +18,13 @@ const logForm = function captureAndLogRegistrationForm(event) {
   console.log(`
   ${fullName.value !== '' ? `Full name: ${fullName.value}` : ''}
   ${email.value !== '' ? `Email: ${email.value}` : ''}
-  ${
-  registrationStatus.value !== ''
+  ${registrationStatus.value !== ''
     ? `Registration status: ${registrationStatus.value}`
-    : ''
-}
+    : ''}
   ${coursesNamesTaken.length > 0 ? `Courses taken: ${coursesNamesTaken}` : ''}
-  ${
-  anythingElse.value !== ''
+  ${anythingElse.value !== ''
     ? `Additional Comments: ${anythingElse.value}`
-    : ''
-}
+    : ''}
   `);
 
   event.preventDefault();
