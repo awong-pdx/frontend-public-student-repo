1b11ead5b8fa0e7fbbd07ffd4f978f22e222ce77
Alessandra Wong <awong@pdx.edu>
2023-05-10 14:24:02 -0700

    finish moving browser router

--- hw3/hw3-react/src/navigation/AppRouter.js
+++ hw3/hw3-react/src/navigation/AppRouter.js
@@ -6,14 +6,12 @@ import HousesPage from '../houses/HousesPage';
 
 function AppRouter() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/home" element={<HomePage />}></Route>
-        <Route path="/search" element={<SearchPage />}></Route>
-        <Route path="/houses" element={<HousesPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/home" element={<HomePage />}></Route>
+      <Route path="/search" element={<SearchPage />}></Route>
+      <Route path="/houses" element={<HousesPage />}></Route>
+    </Routes>
   );
 }
 
